014aef7f7bea4b98e2b5d47985daddf1f8ffbe6b
farukyld <123uzunince123@gmail.com>
2024-01-30 13:58:56 +0300

    update tohost/fromhost test input

--- a_tets_faruk/fromhost_tohost_test/main.c
+++ a_tets_faruk/fromhost_tohost_test/main.c
@@ -4,21 +4,27 @@ int main(int argc, char **argv)
 {
   // printf("Hello World!\n");
   // printf("a icin deger girin\n");
+  char *file_path = "/home/usr1/riscv-isa-sim/"
+                    "a_tets_faruk/spike_link/log/dummy.txt";
+  int a, b;
+  printf("dosya aciliyor: %s\n", file_path);
 
-  int a;
-  FILE *input_file = fopen("/home/usr1/riscv-isa-sim/"
-  "a_tets_faruk/spike_link/log/dummy.txt", "r");
-  fscanf(input_file,"%d", &a);
+  FILE *input_file = fopen(file_path, "r");
 
-  int b;
-  scanf("%d", &b);
+  fscanf(input_file, "%d", &b);
+  printf("dosyadan okunan sayi: %d\n", b);
 
-  a *= b;
+  printf("dosyadan okunan sayiyi "
+         "carpmak icin deger girin\n");
+  scanf("%d", &a);
+  a = a * b;
+
+  printf("carpim sonucu %s dosyasina yaziliyor\n", file_path);
+
   fclose(input_file);
 
   FILE *fp;
-  fp = fopen("/home/usr1/riscv-isa-sim/"
-  "a_tets_faruk/spike_link/log/dummy.txt", "w");
+  fp = fopen(file_path, "w");
   fprintf(fp, "%d", a);
   fclose(fp);
 }
