5b31d1f173ceb76b2d9f618ed74eca8cc9fb08cc
farukyld <123uzunince123@gmail.com>
2025-05-08 11:14:10 +0300

    fix a bug: logging paddr in memory commits was wrong. paddr gets derived from table after cache entry accessed.

--- riscv/mmu.h
+++ riscv/mmu.h
@@ -83,19 +83,18 @@ public:
     bool aligned = (addr & (sizeof(T) - 1)) == 0;
     bool tlb_hit = tlb_load_tag[vpn % TLB_ENTRIES] == vpn;
 
-    reg_t paddr;
     if (likely(!xlate_flags.is_special_access() && aligned && tlb_hit)) {
       res = *(target_endian<T>*)(tlb_data[vpn % TLB_ENTRIES].host_offset + addr);
       paddr = tlb_data[vpn % TLB_ENTRIES].target_offset + addr;
     } else {
       mem_access_info_t access_info = generate_access_info(addr, LOAD, xlate_flags);
-      if (unlikely(proc && proc->get_log_commits_enabled() && (proc->get_log_paddr_only_enabled() || proc->get_log_vaddr_paddr_enabled())))
-        paddr = translate(access_info, sizeof(T));
       load_slow_path(addr, sizeof(T), (uint8_t*)&res, xlate_flags);
     }
-
-    if (unlikely(proc && proc->get_log_commits_enabled()))
+    
+    if (unlikely(proc && proc->get_log_commits_enabled())){
+      auto paddr = translate(access_info, sizeof(T));
       proc->state.log_mem_read.push_back(std::make_tuple(addr,paddr, 0, sizeof(T)));
+    }
 
     return from_target(res);
   }
@@ -129,20 +128,18 @@ public:
     bool aligned = (addr & (sizeof(T) - 1)) == 0;
     bool tlb_hit = tlb_store_tag[vpn % TLB_ENTRIES] == vpn;
 
-    reg_t paddr;
     if (!xlate_flags.is_special_access() && likely(aligned && tlb_hit)) {
       *(target_endian<T>*)(tlb_data[vpn % TLB_ENTRIES].host_offset + addr) = to_target(val);
-      paddr = tlb_data[vpn % TLB_ENTRIES].target_offset + addr;
     } else {
       target_endian<T> target_val = to_target(val);
       mem_access_info_t access_info = generate_access_info(addr, STORE, xlate_flags);
-      if (unlikely(proc && proc->get_log_commits_enabled() && (proc->get_log_paddr_only_enabled() || proc->get_log_vaddr_paddr_enabled())))
-        paddr = translate(access_info, sizeof(T));
       store_slow_path(addr, sizeof(T), (const uint8_t*)&target_val, xlate_flags, true, false);
     }
-
-    if (unlikely(proc && proc->get_log_commits_enabled()))
+    
+    if (unlikely(proc && proc->get_log_commits_enabled())){
+      auto paddr = tlb_data[vpn % TLB_ENTRIES].target_offset + addr;
       proc->state.log_mem_write.push_back(std::make_tuple(addr,paddr, val, sizeof(T)));
+    }
   }
 
   template<typename T>
